46127dfb087e7b95d69f75d68a666d4003e69adb
Alex Thornton <athornton.media@gmail.com>
2021-03-07 16:55:25 -0800

    made timer and start and stop button to control it. Made a description box and a save button that saves the time and descriptoin to a ListBox. Made it so past entries in listbox can be deleted

--- Assignment8/Assignment8/MainWindow.xaml.cs
+++ Assignment8/Assignment8/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Assignment8
 {
@@ -20,9 +21,48 @@ namespace Assignment8
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        private DateTime startTime;
         public MainWindow()
         {
             InitializeComponent();
+
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
+
+
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            Timer.Text = (DateTime.Now - startTime).ToString(@"hh\:mm\:ss");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            startTime = DateTime.Now;
+            dispatcherTimer.Start();
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            dispatcherTimer.Stop();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            SavedBox.Items.Add($"{Timer.Text} {Description.Text}");
+            Description.Text = "";
+            Timer.Text = "00:00:00";
+        }
+
+        private void SavedBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Delete)
+            {
+                SavedBox.Items.Remove(SavedBox.SelectedItem);
+            }
+            
         }
     }
 }
