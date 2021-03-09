117b6470a45627992818bff5524a3d0a83ad6a46
Mia Hunt <miaxyhunt@gmail.com>
2021-03-08 17:32:55 -0800

    feat:Start/Stop button color, visibility
    - Added grid definitions.
    - Consolidated "Start" and "Stop" button, added color change states.
    - Changed "Save" button to "Record", added visibility toggle states.

--- Assignment8/Assignment8/MainWindow.xaml.cs
+++ Assignment8/Assignment8/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,39 +23,64 @@ namespace Assignment8
     /// </summary>
     public partial class MainWindow : Window
     {
-        private DispatcherTimer dispatcherTimer = new DispatcherTimer();
-        private DateTime startTime;
+        private DispatcherTimer dispatcherTimer = new ();
+        private DateTime _StartTime = DateTime.Now;
+        private string _Description;
+        private bool _IsStopped = true;
+        private int lap = 1;
         public MainWindow()
         {
             InitializeComponent();
 
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
             dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
-
-
+            
         }
 
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
-            Timer.Text = (DateTime.Now - startTime).ToString(@"hh\:mm\:ss");
+            Timer.Text = (DateTime.Now - _StartTime).ToString(@"hh\:mm\:ss");
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void StartStop_Click(object sender, RoutedEventArgs e)
         {
-            startTime = DateTime.Now;
-            dispatcherTimer.Start();
+            if (StartStop.Content.ToString() == "Start")
+            {
+                _StartTime = DateTime.Now;
+                dispatcherTimer.Start();
+                _IsStopped = false;
+                Record.Visibility = Visibility.Hidden;
+                Description.Visibility = Visibility.Hidden;
+                // Changes button to show stop
+                StartStop.Content = "Stop";
+                // Changes background color to red
+                StartStop.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#E14518"));
+            }
+            else
+            {
+                dispatcherTimer.Stop();
+                _IsStopped = true;
+                // Changes button to show start
+                StartStop.Content = "Start";
+                // Changes background color back to green
+                StartStop.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#6FA147"));
+                Record.Visibility = Visibility.Visible;
+                Description.Visibility = Visibility.Visible;
+                Description.Text = "Lap " + lap + "  " + _StartTime.ToString("h:mm:ss tt");
+            }
         }
 
-        private void Stop_Click(object sender, RoutedEventArgs e)
+        private void Record_Click(object sender, RoutedEventArgs e)
         {
-            dispatcherTimer.Stop();
-        }
-
-        private void Save_Click(object sender, RoutedEventArgs e)
-        {
-            SavedBox.Items.Add($"{Timer.Text} {Description.Text}");
-            Description.Text = "";
-            Timer.Text = "00:00:00";
+            if (_IsStopped && Timer.Text != "00:00:00")
+            {
+                SavedBox.Items.Add($"{Timer.Text}\t\t{_Description}");
+                lap++;
+                Description.Text = "";
+                Timer.Text = "00:00:00";
+                Record.Visibility = Visibility.Hidden;
+                Description.Visibility = Visibility.Hidden;
+            }
         }
 
         private void SavedBox_KeyDown(object sender, KeyEventArgs e)
@@ -62,7 +89,9 @@ namespace Assignment8
             {
                 SavedBox.Items.Remove(SavedBox.SelectedItem);
             }
-            
+
         }
+        private void Description_TextChanged(object sender, TextChangedEventArgs e) => _Description = Description.Text;
+
     }
 }
